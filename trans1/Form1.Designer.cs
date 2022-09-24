8b19d1b446f107f4dd82c8e488551befea266361
yavuzakan <103066090+yavuzakan@users.noreply.github.com>
2022-09-24 22:27:53 +0300

    Proje dosyası ekle.

---
+++ trans1/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace trans1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.veri = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.veri2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteOldDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(0, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(440, 553);
+            this.textBox1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.veri,
+            this.veri2});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 583);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(440, 158);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // id
+            // 
+            this.id.HeaderText = "*";
+            this.id.Name = "id";
+            this.id.Visible = false;
+            // 
+            // veri
+            // 
+            this.veri.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.veri.HeaderText = "*";
+            this.veri.Name = "veri";
+            // 
+            // veri2
+            // 
+            this.veri2.HeaderText = "*";
+            this.veri2.Name = "veri2";
+            this.veri2.Visible = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(440, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteOldDataToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // deleteOldDataToolStripMenuItem
+            // 
+            this.deleteOldDataToolStripMenuItem.Name = "deleteOldDataToolStripMenuItem";
+            this.deleteOldDataToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.deleteOldDataToolStripMenuItem.Text = "Delete Old Data";
+            this.deleteOldDataToolStripMenuItem.Click += new System.EventHandler(this.deleteOldDataToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(12, 20);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 741);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteOldDataToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn veri;
+        private System.Windows.Forms.DataGridViewTextBoxColumn veri2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ trans1/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trans1
+{
+    public partial class Form1 : Form
+    {
+        database class1 = new database();
+        SQLiteConnection conn;
+        SQLiteCommand cmd;
+        SQLiteDataReader dr;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+            database.Create_db();
+            this.TopMost = true;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            ara();
+            this.Text ="yavuz.akan@gmail.com";
+            
+
+
+        }
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SetClipboardViewer(IntPtr hWndNewViewer);
+
+        public const int WM_DRAWCLIPBOARD = 0x0308;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetClipboardViewer(this.Handle);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg != WM_DRAWCLIPBOARD)
+                return;
+
+            string veri = Clipboard.GetText();
+            //Code To handle Clipboard change event
+            String test = textBox1.Text;
+
+
+
+            if (veri.Equals(test))
+            { 
+            
+            }
+            else
+
+            {
+               // textBox1.Text = veri;
+
+                database.add(veri);
+                cevir();
+                ara();
+
+            }
+            
+            
+
+        }
+
+
+
+
+
+        public void ara()
+        {
+
+            dataGridView1.Rows.Clear();
+            string path = "deneme.db";
+            string cs = @"URI=file:"+Application.StartupPath+"\\deneme.db";
+            var con = new SQLiteConnection(cs);
+            SQLiteDataReader dr;
+            con.Open();
+
+            //string stm = "select * FROM data ORDER BY id ASC  ";
+            //SELECT * FROM (SELECT * FROM graphs WHERE sid=2 ORDER BY id DESC LIMIT 10) g ORDER BY g.id
+            string stm = "select * FROM data ORDER BY id asc LIMIT 10  ";
+            var cmd = new SQLiteCommand(stm, con);
+            dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                dataGridView1.Rows.Insert(0, dr.GetValue(0).ToString(), dr.GetValue(1).ToString(), dr.GetValue(2).ToString());
+
+            }
+
+            con.Close();
+
+            this.dataGridView1.AllowUserToAddRows = false;
+
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            //dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ReadOnly = true;
+
+            // dataGridView1.Columns[0].Visible = false;
+
+            try
+            {
+                DataGridViewRow dataGridViewRow = dataGridView1.Rows[0];
+
+                //textBox1.Text = dataGridViewRow.Cells["veri2"].Value.ToString();
+
+                textBox1.Text =dataGridViewRow.Cells["veri"].Value.ToString() + Environment.NewLine + Environment.NewLine + dataGridViewRow.Cells["veri2"].Value.ToString();
+            }
+            catch
+            { 
+            
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow dataGridViewRow = dataGridView1.Rows[e.RowIndex];
+
+                textBox1.Text =dataGridViewRow.Cells["veri"].Value.ToString() + Environment.NewLine +  Environment.NewLine +  dataGridViewRow.Cells["veri2"].Value.ToString();
+
+
+            }
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow dataGridViewRow = dataGridView1.Rows[e.RowIndex];
+
+
+                textBox1.Text =dataGridViewRow.Cells["veri"].Value.ToString() + Environment.NewLine +  Environment.NewLine +  dataGridViewRow.Cells["veri2"].Value.ToString();
+
+
+
+
+
+            }
+        }
+
+        private void deleteOldDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            DialogResult dialogResult = MessageBox.Show("Sure", "Delete All Notes", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                dataGridView1.Rows.Clear();
+                string path = "deneme.db";
+                string cs = @"URI=file:"+Application.StartupPath+"\\deneme.db";
+                var con = new SQLiteConnection(cs);
+                SQLiteDataReader dr;
+                con.Open();
+
+                //string stm = "select * FROM data ORDER BY id ASC  ";
+                //SELECT * FROM (SELECT * FROM graphs WHERE sid=2 ORDER BY id DESC LIMIT 10) g ORDER BY g.id
+                string stm = "delete from data";
+                var cmd = new SQLiteCommand(stm, con);
+                dr = cmd.ExecuteReader();
+
+                stm = "delete from sqlite_sequence where name='data'";
+                cmd = new SQLiteCommand(stm, con);
+                dr = cmd.ExecuteReader();
+
+                con.Close();
+
+                this.dataGridView1.AllowUserToAddRows = false;
+
+
+                dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                //dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                dataGridView1.ReadOnly = true;
+
+                // dataGridView1.Columns[0].Visible = false;
+
+                textBox1.Text ="";
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do something else
+            }
+        }
+
+
+
+
+        public void cevir()
+        {
+            string q = "";
+            try
+            {
+                String komut = "transpy.exe";
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = "/C " + komut;
+
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardInput = true;
+                process.Start();
+
+
+
+
+                while (!process.HasExited)
+                {
+                    q += process.StandardOutput.ReadToEnd();
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                
+
+            }
+         
+
+
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                notifyIcon1.Visible = true;
+                // notifyIcon1.ShowBalloonTip(1000);
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+    }
+}
